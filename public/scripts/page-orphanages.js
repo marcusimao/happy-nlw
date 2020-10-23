764df1be0477c865d068edb07520efdc304d471e
Marcus Simao <marcus.graciano@gmail.com>
2020-10-22 21:05:32 -0300

    initial commit

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,67 @@
+
+// create map
+const map = L.map('mapid').setView([-27.0075663,-48.673033], 13);
+
+// create and add tileLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+).addTo(map)
+
+
+// create icon
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+
+})
+
+function addMarker({id, name, lat, lng}) {
+
+    // create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent(`${name} <a href="/orphanage?id=${id}" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>`)
+
+// create and make marker
+L.marker([lat,lng], {icon})
+.addTo(map)
+.bindPopup(popup)
+
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+
+    }
+    addMarker(orphanage)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+// tipos de dados
+// String ""
+// Number 01
+// Object {}
+// Boolean true or false
+// Array []
+
---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.0075663",
+        lng: "-48.673033",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1586458132313-b6191b25f567?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/source-404?fit=crop&fm=jpg&h=800&q=60&w=1200"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: " Horário de visita das 8h até 18h",
+        open_on_weekends: "1"
+
+
+    },
+    
+    {
+        id: 2,
+        lat: "-27.0075663",
+        lng: "-48.614033",
+        name: 'Lar das meninos',
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1586458132313-b6191b25f567?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/source-404?fit=crop&fm=jpg&h=800&q=60&w=1200"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: " Horário de visita das 8h até 18h",
+        open_on_weekends: "0"
+    },
+]
